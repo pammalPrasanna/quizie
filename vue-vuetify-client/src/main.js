cf24a787ac2a2d39bec32d19986d61682cc0e247
pammalPrasanna <pammalprasanna@gmail.com>
2021-06-23 09:50:04 +0530

    Minor fix

--- vue-vuetify-client/src/main.js
+++ vue-vuetify-client/src/main.js
@@ -6,7 +6,8 @@ import vuetify from './plugins/vuetify'
 import axios from 'axios'
 
 
-axios.defaults.baseURL = 'http://127.0.0.1:8000'
+// axios.defaults.baseURL = 'http://127.0.0.1:8000'
+axios.defaults.baseURL = 'https://pammalprasanna.pythonanywhere.com'
 Vue.config.productionTip = false
 
 new Vue({
